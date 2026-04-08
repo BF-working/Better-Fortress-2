73776084e322bbe7577002ed5276dd00d993558f
PracticeMedicine <practicemedicine3@gmail.com>
2026-04-09 00:30:37 +0800

    chore(ui): Merge latest changes of the Radial Menu from Lambdagon/source-sdk-2013-radialmenu

--- src/game/client/fc/vgui/radialmenu.h
+++ src/game/client/fc/vgui/radialmenu.h
@@ -7,7 +7,7 @@
 #pragma once
 #endif
 
-#include <vgui_controls/EditablePanel.h>
+#include <vgui_controls/editablepanel.h>
 #include "KeyValues.h"
 
 #define PANEL_RADIAL_MENU			"RadialMenu"
@@ -101,56 +101,5 @@ public:
 void OpenRadialMenu( const char *menuName );
 void CloseRadialMenu( const char *menuName, bool sendCommand = false );
 bool IsRadialMenuOpen( const char *menuName, bool includeFadingOut );
-
-//--------------------------------------------------------------------------------------------------------
-/**
- * Button class that clips its visible bounds to an concave polygon (defined counter-clockwise).
- * An optional second hotspot can be defined for capturing mouse input outside of the visible hotspot.
- */
-class CPolygonButton : public vgui::Button
-{
-	DECLARE_CLASS_SIMPLE( CPolygonButton, vgui::Button );
-
-public:
-
-	CPolygonButton( vgui::Panel *parent, const char *panelName );
-
-	virtual void ApplySettings( KeyValues *data );
-
-	/**
-	 * Clip out cursor positions inside our overall rectangle that are outside our hotspot.
-	 */
-	virtual vgui::VPANEL IsWithinTraverse( int x, int y, bool traversePopups );
-
-	/**
-	 * Perform the standard layout, and scale our hotspot points - they are specified as a 0..1 percentage
-	 * of the button's full size.
-	 */
-	virtual void PerformLayout( void );
-
-	/**
-	 * Center the text in the extent that encompasses our hotspot.
-	 * TODO: allow the res file and/or the individual menu to specify a different center for text.
-	 */
-	virtual void ComputeAlignment( int &tx0, int &ty0, int &tx1, int &ty1 );
-	virtual void PaintBackground( void );
-	virtual void PaintBorder( void );
-	virtual void ApplySchemeSettings( vgui::IScheme *scheme );
-
-	virtual void UpdateHotspots( KeyValues *data );
-
-protected:
-	int m_nWhiteMaterial;
-
-	CUtlVector< Vector2D > m_unscaledHotspotPoints;
-	CUtlVector< Vector2D > m_unscaledVisibleHotspotPoints;
-	vgui::Vertex_t *m_hotspotPoints;
-	int m_numHotspotPoints;
-	vgui::Vertex_t *m_visibleHotspotPoints;
-	int m_numVisibleHotspotPoints;
-
-	Vector2D m_hotspotMins;
-	Vector2D m_hotspotMaxs;
-};
 
 #endif // RADIALMENU_H
